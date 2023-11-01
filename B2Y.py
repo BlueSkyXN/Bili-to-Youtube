e29560d4bc9b6337f0431f77f5e2e95a04be1a55
BlueSkyXN <63384277+BlueSkyXN@users.noreply.github.com>
2023-11-01 17:27:48 +0800

    0.0.9

--- B2Y.py
+++ B2Y.py
@@ -3,14 +3,10 @@ import re
 import sys
 
 def get_aid_and_filename(cmd_args):
-    # 构造命令行参数列表，首个元素为 "BBDown.exe"
     cmd = ["BBDown.exe"]
     cmd.extend(cmd_args)
-    
-    # 运行BBDown.exe并捕获输出
     result = subprocess.run(cmd, capture_output=True, text=True)
     
-    # 提取AID和文件名
     aid_pattern = r"获取aid结束: (\d+)"
     filename_pattern = r"视频标题: (.+)"
     
@@ -24,13 +20,26 @@ def get_aid_and_filename(cmd_args):
     else:
         return None, None
 
+def upload_video(filename, aid):
+    upload_cmd = [
+        "python", "upload_video.py",
+        "-f", filename,
+        "-s", "127.0.0.1:10808",
+        "-d", f"转载自哔哩哔哩，原视频ID为{aid}"
+    ]
+    subprocess.run(upload_cmd)
+
 # 获取命令行参数
 cmd_args = sys.argv[1:]
 
-# 使用示例
+# 获取AID和文件名
 aid, filename = get_aid_and_filename(cmd_args)
+
 if aid and filename:
     print(f"AID: {aid}")
     print(f"文件名: {filename}")
+    
+    # 执行上传操作
+    upload_video(filename, aid)
 else:
     print("无法获取AID或文件名。")
